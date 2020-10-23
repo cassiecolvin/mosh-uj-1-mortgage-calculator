4bb14cb11eb426bb18ff2331627174396039a19d
cassie <cassie@colvindesign.com>
2020-10-23 18:34:31 -0400

    Removed BigDecimal option from Mortgage Calculator as there really is no need for the high precision in financial calculations.

--- src/com/cassiecolvin/Main.java
+++ src/com/cassiecolvin/Main.java
@@ -10,26 +10,15 @@ public class Main {
         Scanner input = new Scanner(System.in);
         MortgageCalculator calculator = new MortgageCalculator();
         String choice;
-        do{
-            System.out.print("Principal: ");
-            double principal = Double.parseDouble(input.nextLine());
-            System.out.print("Annual Interest Rate (%): ");
-            double annualInterestRate = Double.parseDouble(input.nextLine());
-            System.out.print("Period (Years): ");
-            int periodInYears = Integer.parseInt(input.nextLine());
-            System.out.print("[1] - Higher accuracy\n[2] - Standard accuracy\nHigher accuracy or Standard accuracy?: ");
-            choice = input.nextLine();
-            if (choice.equals("1")){
-                //convert input to BD
-                BigDecimal bdPrincipal = new BigDecimal(principal);
-                BigDecimal bdAnnualInterestRate = new BigDecimal(annualInterestRate);
-                System.out.println("Your Monthly Payment Is $"+calculator.calculateMortgage(bdPrincipal,bdAnnualInterestRate,periodInYears).setScale(1,RoundingMode.HALF_UP));
 
-            } else if (choice.equals("2")){
-                System.out.println("Your Monthly Payment Is $"+calculator.calculateMortgage(principal,annualInterestRate,periodInYears));
-            } else {
-                System.out.print("Invalid Choice, Please try again");
-            }
-        } while(!choice.equals("1") && !choice.equals("2"));
+        System.out.print("Principal: ");
+        double principal = Double.parseDouble(input.nextLine());
+        System.out.print("Annual Interest Rate (%): ");
+        double annualInterestRate = Double.parseDouble(input.nextLine());
+        System.out.print("Period (Years): ");
+        int periodInYears = Integer.parseInt(input.nextLine());
+
+        System.out.println("Your Monthly Payment Is $"+calculator.calculateMortgage(principal,annualInterestRate,periodInYears));
+
     }
 }
--- src/com/cassiecolvin/MortgageCalculator.java
+++ src/com/cassiecolvin/MortgageCalculator.java
@@ -10,29 +10,11 @@ public class MortgageCalculator {
 
     }
 
-    public BigDecimal calculateMortgage(BigDecimal principal, BigDecimal annualInterestRate, int periodInYears){
-        BigDecimal bd1 = new BigDecimal("1.0");
-
-        //Perform initial calculations to scale of monthly payments vs annual
-        BigDecimal monthlyInterestRate = calculateMonthlyInterestRate(annualInterestRate);
-        int periodInMonths = periodInYears*12;
-
-        //Perform math to calculate monthly mortgage rate
-        BigDecimal subCalculation1 = (monthlyInterestRate.add(bd1)).pow(periodInMonths, MathContext.DECIMAL128);
-        return principal.multiply(monthlyInterestRate.multiply(subCalculation1,MathContext.DECIMAL128).divide(subCalculation1.subtract(bd1),MathContext.DECIMAL128),MathContext.DECIMAL128);
-    }
-
     public double calculateMortgage(double principal, double annualInterestRate,int periodInYears){
         double monthlyInterestRate = annualInterestRate/100/12;
         int periodInMonths = periodInYears*12;
 
         double subCalculation1 = Math.pow(monthlyInterestRate+1.0,periodInMonths);
         return Math.round(principal*((monthlyInterestRate*subCalculation1)/(subCalculation1-1.0))*10.0)/10.0;
-    }
-
-    public BigDecimal calculateMonthlyInterestRate(BigDecimal annualInterestRate){
-        BigDecimal outOf100 = new BigDecimal("100.0");
-        BigDecimal monthsInYear = new BigDecimal("12.0");
-        return (annualInterestRate.divide(outOf100)).divide(monthsInYear);
     }
 }
