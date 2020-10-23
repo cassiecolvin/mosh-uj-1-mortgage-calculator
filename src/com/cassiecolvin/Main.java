f795102193f378228f30e1967e62327810104676
cassie <cassie@colvindesign.com>
2020-10-23 19:11:12 -0400

    Added in the ability to calculate a Balloon Loan. Working out issues with formula

--- src/com/cassiecolvin/Main.java
+++ src/com/cassiecolvin/Main.java
@@ -11,14 +11,33 @@ public class Main {
         MortgageCalculator calculator = new MortgageCalculator();
         String choice;
 
-        System.out.print("Principal: ");
-        double principal = Double.parseDouble(input.nextLine());
-        System.out.print("Annual Interest Rate (%): ");
-        double annualInterestRate = Double.parseDouble(input.nextLine());
-        System.out.print("Period (Years): ");
-        int periodInYears = Integer.parseInt(input.nextLine());
+        do {
+            System.out.print("[1] Fixed Rate Mortgage\n[2] Ballon Loan\nWhat type of mortgage loan do you have?: ");
+            choice = input.nextLine();
+            if (choice.equals("1")){
+                System.out.print("Principal: ");
+                double principal = Double.parseDouble(input.nextLine());
+                System.out.print("Annual Interest Rate (%): ");
+                double annualInterestRate = Double.parseDouble(input.nextLine());
+                System.out.print("Period (Years): ");
+                int periodInYears = Integer.parseInt(input.nextLine());
+                System.out.println(calculator.displayFixedRateMortgageMonthlyPayment(principal,annualInterestRate,periodInYears));
+            }
+            else if (choice.equals("2")){
+                System.out.print("Principal: ");
+                double principal = Double.parseDouble(input.nextLine());
+                System.out.print("Annual Interest Rate (%): ");
+                double annualInterestRate = Double.parseDouble(input.nextLine());
+                System.out.print("Calculated Period (Years): ");
+                int calculatedPeriodInYears = Integer.parseInt(input.nextLine());
+                System.out.print("Actual Period (Years): ");
+                int actualPeriodInYears = Integer.parseInt(input.nextLine());
+                System.out.println(calculator.displayBalloonLoanInfo(principal,annualInterestRate,calculatedPeriodInYears,actualPeriodInYears));
+            }
+            else
+                System.out.println("Invalid Choice! Please try again...");
 
-        System.out.println("Your Monthly Payment Is $"+calculator.calculateMortgage(principal,annualInterestRate,periodInYears));
+        } while (!choice.equals("1") && !choice.equals("2"));
 
     }
 }
--- src/com/cassiecolvin/MortgageCalculator.java
+++ src/com/cassiecolvin/MortgageCalculator.java
@@ -10,11 +10,26 @@ public class MortgageCalculator {
 
     }
 
-    public double calculateMortgage(double principal, double annualInterestRate,int periodInYears){
+    private double calculateLoanMonthlyPayment(double principal, double annualInterestRate,int periodInYears){
         double monthlyInterestRate = annualInterestRate/100/12;
         int periodInMonths = periodInYears*12;
 
         double subCalculation1 = Math.pow(monthlyInterestRate+1.0,periodInMonths);
         return Math.round(principal*((monthlyInterestRate*subCalculation1)/(subCalculation1-1.0))*10.0)/10.0;
+    }
+
+    private double calculateRemainingBalloonLoanAmountDue(double principal, double monthlyPayment,int actualPeriodInYears){
+        return Math.round((principal-(actualPeriodInYears*12)*monthlyPayment)*10.0)/10.0;
+    }
+
+    public String displayFixedRateMortgageMonthlyPayment(double principal, double annualInterestRate, int periodInYears){
+        return "Your monthly payment is $"+calculateLoanMonthlyPayment(principal,annualInterestRate,periodInYears);
+    }
+
+    public String displayBalloonLoanInfo(double principal, double annualInterestRate,int calculatedPeriodInYears,int actualPeriodInYears){
+        double monthlyPayment = calculateLoanMonthlyPayment(principal,annualInterestRate,calculatedPeriodInYears);
+        double remainingBalance = calculateRemainingBalloonLoanAmountDue(principal,monthlyPayment,actualPeriodInYears);
+
+        return "Your monthly balloon loan payment for a "+actualPeriodInYears+" year loan is $"+monthlyPayment+" with a remaining balance of $"+remainingBalance;
     }
 }
